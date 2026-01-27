79a20f3f28b58a4012f696690fef32b270fd8b6a
zac-williamson <blorktronics@gmail.com>
2026-01-27 14:50:32 +0000

    renamed contract to FogOfWarChess. working script that can create a game, play two moves and sync to game state client-side

--- index.ts
+++ index.ts
@@ -1,87 +1,14 @@
-import {  type MoveEvent, BobTokenContract } from "./artifacts/BobToken.js";
+import {  type MoveEvent, FogOfWarChessContract } from "./artifacts/FogOfWarChess.ts";
 import { AztecAddress } from "@aztec/aztec.js/addresses";
 import { createAztecNodeClient } from "@aztec/aztec.js/node";
 import { getInitialTestAccountsData } from "@aztec/accounts/testing";
 import { TestWallet } from "@aztec/test-wallet/server";
-import { openTmpStore } from "@aztec/kv-store/lmdb";
 import { getDecodedPublicEvents } from "@aztec/aztec.js/events";
 
-
-// export async function initGameState() {
-//   try {
-//     let gameState = await empty_game_state();
-//     let whiteState = await empty_white_state();
-//     let blackState = await empty_black_state();
-
-//     // NOTE: in a real game these secrets should be random and not shared.
-//     whiteState.encrypt_secret = "1";
-//     whiteState.mask_secret = "2";
-//     blackState.encrypt_secret = "3";
-//     blackState.mask_secret = "4";
-//     let whiteEncryptSecret = "1";
-//     let whiteMaskSecret = "2";
-//     let blackEncryptSecret = "3";
-//     let blackMaskSecret = "4";
-//     gameState = await commit_to_user_secrets(
-//       gameState,
-//       whiteEncryptSecret,
-//       whiteMaskSecret,
-//       "0" // 0 = white
-//     );
-//     gameState = await commit_to_user_secrets(
-//       gameState,
-//       blackEncryptSecret,
-//       blackMaskSecret,
-//       "1" // 1 = black
-//     );
-
-//     return { gameState, whiteState, blackState };
-//   } catch (e) {
-//     console.log("error? ", e);
-//   }
-// }
-
-// at the top of your file
-async function getBalances(
-  contract: BobTokenContract,
-  aliceAddress: AztecAddress,
-  bobAddress: AztecAddress,
-) {
-  Promise.all([
-    contract.methods
-      .public_balance_of(aliceAddress)
-      .simulate({ from: aliceAddress }),
-    contract.methods
-      .private_balance_of(aliceAddress)
-      .simulate({ from: aliceAddress }),
-    contract.methods
-      .public_balance_of(bobAddress)
-      .simulate({ from: bobAddress }),
-    contract.methods
-      .private_balance_of(bobAddress)
-      .simulate({ from: bobAddress }),
-  ]).then(
-    ([
-      alicePublicBalance,
-      alicePrivateBalance,
-      bobPublicBalance,
-      bobPrivateBalance,
-    ]) => {
-      console.log(
-        `📊 Alice has ${alicePublicBalance} public BOB tokens and ${alicePrivateBalance} private BOB tokens`,
-      );
-      console.log(
-        `📊 Bob's Clinic has ${bobPublicBalance} public BOB tokens and ${bobPrivateBalance} private BOB tokens`,
-      );
-    },
-  );
-}
 
 async function main() {
   // Connect to local network
   const node = createAztecNodeClient("http://localhost:8080");
-
-  const store = await openTmpStore();
 
   const wallet = await TestWallet.create(node);
 
@@ -104,22 +31,14 @@ async function main() {
   const aliceAddress = (await aliceAccount.getAccount()).getAddress();
   const bobClinicAddress = (await bobClinicAccount.getAccount()).getAddress();
 
-  const bobToken = await BobTokenContract.deploy(wallet)
+  const bobToken = await FogOfWarChessContract.deploy(wallet)
     .send({ from: giggleAddress })
     .deployed();
 
   let whiteState = await bobToken.methods
     .__empty_white_state()
     .simulate({ from: giggleAddress });
-  console.log("white state ", whiteState);
 
-//   console.log("about to call method that errors?");
-//   await bobToken.methods
-//     .args_hash_err(whiteState)
-//     .send({ from: giggleAddress })
-//     .wait();
-
-  console.log("creating game state");
   let gameState = await bobToken.methods
     .__empty_game_state()
     .simulate({ from: giggleAddress });
@@ -127,7 +46,6 @@ async function main() {
   let blackState = await bobToken.methods
     .__empty_black_state()
     .simulate({ from: giggleAddress });
-  console.log("black state");
 
   // NOTE: in a real game these secrets should be random and not shared.
   whiteState.encrypt_secret = 1;
@@ -138,9 +56,7 @@ async function main() {
   let whiteMaskSecret = 2;
   let blackEncryptSecret = 3;
   let blackMaskSecret = 4;
-  console.log("commit to white secrets");
 
-  console.log("game state? ", gameState);
   gameState = await bobToken.methods
     .__commit_to_user_secrets(
       gameState,
@@ -149,11 +65,8 @@ async function main() {
       0, // 0 = white
     )
     .simulate({ from: giggleAddress });
-  console.log("commit to black secrets");
 
-  console.log("GAME STATE WHITE SECRET HASH = ", gameState.mpc_state.user_encrypt_secret_hashes[0].toString(16));
-  console.log("GAME STATE WHITE MASK HASH = " , gameState.mpc_state.user_mask_secret_hashes[0].toString(16));
-  gameState = await bobToken.methods
+    gameState = await bobToken.methods
     .__commit_to_user_secrets(
       gameState,
       blackEncryptSecret,
@@ -161,29 +74,12 @@ async function main() {
       1, // 1 = black
     )
     .simulate({ from: giggleAddress });
-  console.log("GAME STATE 2 WHITE SECRET HASH = ", gameState.mpc_state.user_encrypt_secret_hashes[0].toString(16));
-  console.log("GAME STATE 2 WHITE MASK HASH = " , gameState.mpc_state.user_mask_secret_hashes[0].toString(16));
-  console.log("GAME STATE BLACK SECRET HASH = ", gameState.mpc_state.user_encrypt_secret_hashes[1].toString(16));
-  console.log("GAME STATE BLACK MASK HASH = " , gameState.mpc_state.user_mask_secret_hashes[1].toString(16));
 
-  // at this point we sort of have an initial state we can play with?
-  console.log("game state? ", gameState);
 
-  console.log(
-    "secret hashes? ",
-    gameState.mpc_state.user_encrypt_secret_hashes,
-  );
-
-  //   console.log("white state secrets ", whiteState.)
   let move = await bobToken.methods
     .__create_move(0, 1, 0, 3)
     .simulate({ from: giggleAddress });
 
-  let new_user_state = await bobToken.methods
-    .update_user_state_from_move(true, whiteState, move, 0)
-    .simulate({ from: giggleAddress });
-
-  console.log("new user state = ", new_user_state);
 
   await bobToken.methods
     .create_game_private(whiteState.encrypt_secret, whiteState.mask_secret, 3)
@@ -195,98 +91,42 @@ async function main() {
     .send({ from: aliceAddress })
     .wait();
 
-      console.log("about to call method that errors?");
   let receipt = await bobToken.methods
     .make_move_white_private(0, gameState, whiteState, move)
     .send({ from: giggleAddress })
     .wait();
 
 
-// OK next question. Can I convert this event data into a struct? can I just emit the struct?
-    console.log("fee = ", receipt.transactionFee);
-//   let receipt = await bobToken.methods
-//     .make_move_white_private(0, gameState, whiteState, move)
-//     .profile()
-//     .send({ from: giggleAddress })
-//     .wait();
-
-  //         const logs = await aztecNode.getPublicLogs({ txHash: receipt.txHash });
-  // const rawFields = logs.logs[0].log.getEmittedFields(); // Fr[]
-  console.log(" move hash?", receipt.txHash);
-//   await bobToken.methods
-//     .mint_public(aliceAddress, 100n)
-//     .send({ from: giggleAddress })
-//     .wait();
-
-const logs = await node.getPublicLogs({ txHash: receipt.txHash });
-const rawFields = logs.logs[0].log.getEmittedFields(); // Fr[]
-
-console.log("log? ", logs.logs[0].log.toHumanReadable());
-console.log("raw log? ", rawFields);
-  receipt.blockNumber;
 const collectedEvent0s = await getDecodedPublicEvents<MoveEvent>(
   node,
-  BobTokenContract.events.MoveEvent,
+  FogOfWarChessContract.events.MoveEvent,
   receipt.blockNumber!,
   receipt.blockNumber! + 1,
 );
-
-//   let foo = await bobToken.methods
-//     .test_user_output_state(logs.logs[0].log);
-//     .send({ from: giggleAddress })
-//     .wait();
+let aliceUserOutputState = collectedEvent0s[0].state;
 
 
-// Get logs for a block range
-// const logFilter = {
-//   fromBlock: startBlock,
-//   toBlock: endBlock,
-// };
-// const publicLogs = (await aztecNode.getPublicLogs(logFilter)).logs;
+  let new_white_user_state = await bobToken.methods
+    .__update_user_state_from_move(true, whiteState, move, 0)
+    .simulate({ from: giggleAddress });
 
 
+    let newGameState = await bobToken.methods.__update_game_state_from_move(gameState, aliceUserOutputState, 0).simulate({from: giggleAddress});
 
-//   await bobToken.methods
-//     .transfer_public(bobClinicAddress, 10n)
-//     .send({ from: aliceAddress })
-//     .wait();
+    let  bobNewUserState =
+        await bobToken.methods.__consume_opponent_move(newGameState, blackState, 1)
+        .simulate({ from: giggleAddress });
+        
 
-//   // ...etc
-//   await bobToken.methods
-//     .mint_public(aliceAddress, 100n)
-//     .send({ from: giggleAddress })
-//     .wait();
-//   await getBalances(bobToken, aliceAddress, bobClinicAddress);
+    let bobMove = await bobToken.methods
+    .__create_move(4, 6, 4, 4)
+    .simulate({ from: giggleAddress });
 
-//   await bobToken.methods
-//     .transfer_public(bobClinicAddress, 10n)
-//     .send({ from: aliceAddress })
-//     .wait();
-//   await getBalances(bobToken, aliceAddress, bobClinicAddress);
 
-//   await bobToken.methods
-//     .public_to_private(90n)
-//     .send({ from: aliceAddress })
-//     .wait();
-//   await getBalances(bobToken, aliceAddress, bobClinicAddress);
-
-//   await bobToken.methods
-//     .transfer_private(bobClinicAddress, 50n)
-//     .send({ from: aliceAddress })
-//     .wait();
-//   await getBalances(bobToken, aliceAddress, bobClinicAddress);
-
-//   await bobToken.methods
-//     .private_to_public(10n)
-//     .send({ from: aliceAddress })
-//     .wait();
-//   await getBalances(bobToken, aliceAddress, bobClinicAddress);
-
-//   await bobToken.methods
-//     .mint_private(aliceAddress, 100n)
-//     .send({ from: giggleAddress })
-//     .wait();
-// await getBalances(bobToken, aliceAddress, bobClinicAddress);
+  let bobReceipt = await bobToken.methods
+    .make_move_black_private(0, newGameState, bobNewUserState, bobMove)
+    .send({ from: giggleAddress })
+    .wait();
 }
 
 main().catch(console.error);
